e8834454a015c8b2a4b322bf887d1dd4160d32bd
Vasiliy Zemlyanov <angeldaren@gmail.com>
2014-06-27 21:43:09 +0400

    update pandas_notes.py

--- pandas_notes.py
+++ pandas_notes.py
@@ -13,5 +13,24 @@ data['nameFirst']
 # Add new column using old columns
 data['new_column'] = data['weight'] + data['height']
 
+# Filter data by column.
+data = data[data.colour = 'green']
+
+# Create new row with values equal difference between current and previous value of another row
+data['diff'] = (data['money'] - data['money'].shift(1)).fillna(1)
+
 # Save data to csv
 data.to_csv('new_file.csv')
+
+# You can use SQL queries in pandas
+import pandasql
+q = """
+    SELECT * FROM data LIMIT 30 WHERE country = "russia"
+    """
+pandasql.sqldf(q.lower(), locals())
+
+# Describe dataframe
+data.describe()
+
+# Fill NA values with the mean
+data['weight'] = data['weight'].fillna(numpy.mean(data['weight']))
